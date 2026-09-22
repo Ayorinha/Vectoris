48546b11fb766c6ed8e69ed967affa5f7d2d8b2a
ANDERSON AYORA <ayora.anderson@gmail.com>
2026-09-22 15:20:36 -0300

    fix: repair test source formatting

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,3 +1,6 @@
-from vectoris.core import *
+from vectoris.core import InMemoryVectorIndex, Vector
 
-def test_vector():\n i=InMemoryVectorIndex();i.add(Vector("a",(1,0)));assert i.search((1,0))[0][0]=="a"
+def test_vector():
+    index = InMemoryVectorIndex()
+    index.add(Vector("a", (1, 0)))
+    assert index.search((1, 0))[0][0] == "a"
